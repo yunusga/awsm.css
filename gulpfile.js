2f7efdd78d82260f166117cd866ed3680beb8abb
Yunus Gaziyev <yunusga@yandex.ru>
2024-07-09 07:17:06 +0500

    It's alive

--- gulpfile.js
+++ gulpfile.js
@@ -1,86 +1,90 @@
 /* requires */
 
-var gulp		= require('gulp'),
-	util		= require('gulp-util'),
-	browserSync = require('browser-sync'),
-	cache 		= require('gulp-cached'),
-	fileinclude = require('gulp-file-include'),
-	rename		= require('gulp-rename'),
-	concat		= require('gulp-concat'),
-	filter 		= require('gulp-filter'),
-	rimraf		= require('rimraf'),
+const { src, dest, series, watch }        = require('gulp');
+const util		  = require('gulp-util');
+const browserSync = require('browser-sync');
+const cache       = require('gulp-cached');
+const fileinclude = require('gulp-file-include');
+const rename      = require('gulp-rename');
+const concat      = require('gulp-concat');
+const filter      = require('gulp-filter');
+const rimraf      = require('rimraf');
 
-	autoprefixr	= require('autoprefixer'),
-	nocomments	= require('postcss-discard-comments'),
-	sass		= require('gulp-sass'),
-	postcss 	= require('gulp-postcss'),
-	minifyCSS	= require('gulp-minify-css');
+const autoprefixer = require('autoprefixer');
+const nocomments   = require('postcss-discard-comments');
+const sass         = require('gulp-sass')(require('node-sass'));
+const postcss      = require('gulp-postcss');
+const minifyCSS    = require('gulp-minify-css');
 
 /* paths */
 
-var input = {
-		html: 'dev/example/**/*.html',
-		scss: 'dev/scss/**/*.scss',
-		images: 'dev/example/images/*'
-	},
-	output = {
-		dist: 'dist',
-		main: 'example',
-		css: 'example/css',
-		images: 'example/images'
-	};
+const input = {
+	html: 'dev/example/**/*.html',
+	scss: 'dev/scss/**/*.scss',
+	images: 'dev/example/images/*'
+};
 
-gulp.task('default', ['build', 'server', 'watch']);
+const output = {
+	dist: 'dist',
+	main: 'example',
+	css: 'example/css',
+	images: 'example/images'
+};
 
-gulp.task('build', ['markup', 'styles', 'images']);
-
-gulp.task('markup', function() {
-	gulp.src(input.html)
+function markup() {
+	return src(input.html)
 		.pipe(fileinclude().on('error', util.log))
 		.pipe(cache('htmling'))
 		.pipe(filter(['*', '!dev/example/includes']))
-		.pipe(gulp.dest(output.main))
+		.pipe(dest(output.main))
 		.pipe(browserSync.stream());
-});
+};
 
-gulp.task('styles', function() {
-	gulp.src(input.scss)
+function styles() {
+	return src(input.scss)
 		.pipe(concat('awsm.scss'))
 		.pipe(sass().on('error', util.log))
 
-		.pipe(postcss([ autoprefixr({ browsers: [ "> 1%" ] }), nocomments() ]))
-		.pipe(gulp.dest(output.css))
-		.pipe(gulp.dest(output.dist))
+		.pipe(postcss([ autoprefixer(), nocomments() ]))
+		.pipe(dest(output.css))
+		.pipe(dest(output.dist))
 
 		.pipe(minifyCSS())
 		.pipe(rename('awsm.min.css'))
-		.pipe(gulp.dest(output.css))
-		.pipe(gulp.dest(output.dist))
+		.pipe(dest(output.css))
+		.pipe(dest(output.dist))
 
 		.pipe(browserSync.stream());
-});
+};
 
-gulp.task('images', function() {
-	gulp.src(input.images) 
-		.pipe(gulp.dest(output.images))
+function images() {
+	return src(input.images)
+		.pipe(dest(output.images))
 		.pipe(browserSync.stream());
-});
+};
 
-gulp.task('server', function() {
+function server(cb) {
 	browserSync.init({
 		server: output.main,
 		open: false,
 		browser: "browser",
 		reloadOnRestart: true
 	});
-});
 
-gulp.task('watch', function() {
-	gulp.watch(input.html, ['markup']);
-	gulp.watch(input.scss, ['styles']);
-	gulp.watch(input.images, ['images']);
-});
+	cb();
+};
 
-gulp.task('clean', function(cb) {
+function watchFiles(cb) {
+	watch(input.html, markup);
+	watch(input.scss, styles);
+	watch(input.images, images);
+
+	cb();
+};
+
+function clean(cb) {
 	rimraf(output.main, cb);
-});
+};
+
+exports.build = series(markup, styles, images);
+exports.default = series(markup, styles, images, watchFiles, server);
